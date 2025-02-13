2270b36310b08696402688187a9182927e3c6222
hithesh-lal <hithesh.lal@experionglobal.com>
2025-02-13 11:07:39 +0530

    Updated .gitignore to exclude environment files

--- Harmonizer/src/environments/environment.development.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: false,
-    googleClientId: '211496683085-mgml1rhsdnaek9g3ga6ln3ejshubij9k.apps.googleusercontent.com',
-   
-};
--- Harmonizer/src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: false,
-  googleClientId: 'your-development-client-id.apps.googleusercontent.com',
-  apiUrl: 'https://localhost:7119',
-};
