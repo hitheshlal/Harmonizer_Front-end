9dcbc1c03b8f275e352b5cb05e3b9f8a9595a4ca
hithesh-lal <hithesh.lal@experionglobal.com>
2025-03-05 14:52:47 +0530

    build commands from vercel platform

--- Harmonizer/src/env.js
+++ Harmonizer/src/env.js
@@ -1,9 +1,12 @@
 (function(window) {
   window["env"] = window["env"] || {};
   // Environment variables
-  window["env"]["apiUrl"] = "https://localhost:7119";
+  window["env"]["apiUrl"] = "http://localhost:4200/";
   window["env"]["googleClientId"] = "211496683085-mgml1rhsdnaek9g3ga6ln3ejshubij9k.apps.googleusercontent.com";
 
 })(this);
 
 
+
+
+// https://localhost:7119
